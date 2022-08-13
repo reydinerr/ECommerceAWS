e9d84bb2e428473ad2e689d0fc1918b5d50b4185
redusav <reydiner@protonmail.com>
2022-08-13 17:08:15 -0300

    build: first commit

--- bin/e_commerce_aws.ts
+++ bin/e_commerce_aws.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { ECommerceAwsStack } from "../lib/e_commerce_aws-stacks";
+import { ECommerceAwsStack } from "../lib/e_commerce_aws-stack";
 
 const app = new cdk.App();
 new ECommerceAwsStack(app, "ECommerceAwsStack", {
