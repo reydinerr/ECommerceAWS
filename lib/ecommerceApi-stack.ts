f523e060b8e11dc8a11e15c395e09e5c392730ee
redusav <reydiner@protonmail.com>
2022-08-14 11:10:09 -0300

    build: added service ecommerceAPI-stack

---
+++ lib/ecommerceApi-stack.ts
@@ -0,0 +1,25 @@
+import * as cdk from 'aws-cdk-lib';
+import * as lambdaNodeJS from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as cwlogs from 'aws-cdk-lib/aws-logs';
+import { Construct } from 'constructs';
+
+interface ECommerceApiStackProps extends cdk.StackProps {
+  productsFatchHandler: lambdaNodeJS.NodejsFunction;
+}
+export class ECommerceApiStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: ECommerceApiStackProps) {
+    super(scope, id, props);
+
+    const api = new apigateway.RestApi(this, 'ECommerceApi', {
+      restApiName: 'ECommerceApi',
+    });
+
+    const productsFetchIntegration = new apigateway.LambdaIntegration(
+      props.productsFatchHandler,
+    );
+
+    const productsResource = api.root.addResource('products');
+    productsResource.addMethod('GET', productsFetchIntegration);
+  }
+}
