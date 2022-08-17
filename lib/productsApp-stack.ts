4464b5ea2f64f972e54a47b72747631406858031
redusav <reydiner@protonmail.com>
2022-08-17 01:00:51 -0300

    feat: add dynamodb at project

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -1,11 +1,28 @@
 import * as lambdaNodeJS from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as cdk from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+//https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/HowItWorks.NamingRulesDataTypes.html#HowItWorks.DataTypes
 import { Construct } from 'constructs';
+import { strict } from 'assert';
 
 export class ProductsAppStack extends cdk.Stack {
   readonly productsFetchHandler: lambdaNodeJS.NodejsFunction;
+  readonly productsDbd: dynamodb.Table;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    this.productsDbd = new dynamodb.Table(this, 'ProductsDdb', {
+      tableName: 'Products',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      partitionKey: {
+        name: 'id',
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PROVISIONED,
+      readCapacity: 1,
+      writeCapacity: 1,
+    });
     this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(
       this,
       'ProductsFetchFunction',
