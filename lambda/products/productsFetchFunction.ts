0715c03b93caf77b6f08b2c9179b8183409124c9
redusav <reydiner@protonmail.com>
2022-08-22 14:27:56 -0300

    feat: add route get

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -1,30 +1,44 @@
-import { APIGatewayProxyResult, APIGatewayProxyEvent, Context } from "aws-lambda";
+import {
+  APIGatewayProxyResult,
+  APIGatewayProxyEvent,
+  Context,
+} from 'aws-lambda';
 
-export async function handler(event: APIGatewayProxyEvent,
-  context: Context): Promise<APIGatewayProxyResult> {
-
+export async function handler(
+  event: APIGatewayProxyEvent,
+  context: Context,
+): Promise<APIGatewayProxyResult> {
   const lambdaRequestId = context.awsRequestId;
   const apiRequestId = event.requestContext.requestId;
 
-  console.log(`API Gateway RequestId: ${apiRequestId} - lambda RequestId ${lambdaRequestId}`);
+  console.log(
+    `API Gateway RequestId: ${apiRequestId} - lambda RequestId ${lambdaRequestId}`,
+  );
 
   const method = event.httpMethod;
-  if (event.resource === "/products") {
+  if (event.resource === '/products') {
     if (event.httpMethod === 'GET') {
       console.log('GET');
 
       return {
         statusCode: 200,
         body: JSON.stringify({
-          message: "GET Products - OK"
-        })
-      }
+          message: 'GET Products - OK',
+        }),
+      };
     }
+  } else if (event.resource === '/products/{id}') {
+    const productId = event.pathParameters!.id as string;
+    console.log(`GET/products/${productId}`);
+    return {
+      statusCode: 200,
+      body: `GET /products/${productId}`,
+    };
   }
   return {
     statusCode: 400,
     body: JSON.stringify({
-      message: "Invalid request"
-    })
-  }
+      message: 'Invalid request',
+    }),
+  };
 }
