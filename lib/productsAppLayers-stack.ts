2a65ec8143bbcc4cde7c3c4226a12d9263b407f3
reydinerr <reydiner@protonmail.com>
2022-09-06 13:30:30 -0300

    refactor: products app layer

--- lib/productsAppLayers-stack.ts
+++ lib/productsAppLayers-stack.ts
@@ -7,6 +7,15 @@ export class ProductsAppLayersStack extends cdk.Stack {
   readonly productsLayers: lambda.LayerVersion;
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    this.productsLayers = new lambda.LayerVersion(this, 'ProductsLayer', {});
+    this.productsLayers = new lambda.LayerVersion(this, 'ProductsLayer', {
+      code: lambda.Code.fromAsset('lambda/products/layers/productsLayer'),
+      compatibleRuntimes: [lambda.Runtime.NODEJS_16_X],
+      layerVersionName: "ProductsLayer",
+      removalPolicy: cdk.RemovalPolicy.RETAIN
+    });
+    new ssm.StringParameter(this, "ProductsLayerVersionArn", {
+      parameterName: "ProductsLayerVersionArn",
+      stringValue: this.productsLayers.layerVersionArn
+    })
   }
 }
