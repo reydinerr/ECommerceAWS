9f2ec1a70be042e7a802ed345d14481e2fb04a57
reydinerr <reydiner@protonmail.com>
2022-09-08 20:51:33 -0300

    feat: update functions handler

--- lambda/products/productsAdminFunction.ts
+++ lambda/products/productsAdminFunction.ts
@@ -1,9 +1,18 @@
-import { JsonFileLogDriver } from 'aws-cdk-lib/aws-ecs';
 import {
   APIGatewayProxyResult,
   APIGatewayProxyEvent,
   Context,
 } from 'aws-lambda';
+import {
+  Product,
+  ProductRepository,
+} from './layers/productsLayer/nodejs/productRepository';
+import { DynamoDB } from 'aws-sdk';
+
+const productsDdb = process.env.PRODUCTS_DDB!;
+const ddbClient = new DynamoDB.DocumentClient();
+
+const productRepository = new ProductRepository(ddbClient, productsDdb);
 
 export async function handler(
   event: APIGatewayProxyEvent,
@@ -17,27 +26,49 @@ export async function handler(
   );
   if (event.resource === '/products') {
     console.log('POST /products');
+    const product = JSON.parse(event.body!) as Product;
+    const productCreated = await productRepository.create(product);
     return {
       statusCode: 201,
-      body: 'POST /products OK',
+      body: JSON.stringify(productCreated),
     };
   } else if (event.resource === '/products/{id}') {
     const productId = event.pathParameters!.id as string;
     if (event.httpMethod === 'PUT') {
       console.log(`PUT /products/${productId}`);
-      return {
-        statusCode: 201,
-        body: `PUT /products/${productId}`,
-      };
+      const product = JSON.parse(event.body!) as Product;
+      try {
+        const productUpdated = await productRepository.updateProduct(
+          productId,
+          product,
+        );
+        return {
+          statusCode: 200,
+          body: JSON.stringify(productUpdated),
+        };
+      } catch (ConditionalCheckFailedException) {
+        return {
+          statusCode: 404,
+          body: 'Product not found.',
+        };
+      }
     } else if (event.httpMethod === 'DELETE') {
       console.log(`DELETE /products/${productId}`);
-      return {
-        statusCode: 200,
-        body: `DELETE /products/${productId}`,
-      };
+      try {
+        const product = await productRepository.deleteProduct(productId);
+        return {
+          statusCode: 200,
+          body: JSON.stringify(product),
+        };
+      } catch (error) {
+        console.error((<Error>error).message);
+        return {
+          statusCode: 404,
+          body: (<Error>error).message,
+        };
+      }
     }
   }
-
   return {
     statusCode: 400,
     body: 'Bad Request',
--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -3,6 +3,13 @@ import {
   APIGatewayProxyEvent,
   Context,
 } from 'aws-lambda';
+import { ProductRepository } from './layers/productsLayer/nodejs/productRepository';
+import { DynamoDB } from 'aws-sdk';
+
+const productsDdb = process.env.PRODUCTS_DDB!;
+const ddbClient = new DynamoDB.DocumentClient();
+
+const productRepository = new ProductRepository(ddbClient, productsDdb);
 
 export async function handler(
   event: APIGatewayProxyEvent,
@@ -18,22 +25,32 @@ export async function handler(
   const method = event.httpMethod;
   if (event.resource === '/products') {
     if (event.httpMethod === 'GET') {
-      console.log('GET');
+      console.log('GET / products');
+
+      const products = await productRepository.getAllProducts();
 
       return {
         statusCode: 200,
-        body: JSON.stringify({
-          message: 'GET Products - OK',
-        }),
+        body: JSON.stringify({ products }),
       };
     }
   } else if (event.resource === '/products/{id}') {
     const productId = event.pathParameters!.id as string;
     console.log(`GET/products/${productId}`);
-    return {
-      statusCode: 200,
-      body: `GET /products/${productId}`,
-    };
+
+    try {
+      const product = await productRepository.getProductById(productId);
+      return {
+        statusCode: 200,
+        body: JSON.stringify(product),
+      };
+    } catch (error) {
+      console.error((<Error>error).message);
+      return {
+        statusCode: 404,
+        body: (<Error>error).message,
+      };
+    }
   }
   return {
     statusCode: 400,
