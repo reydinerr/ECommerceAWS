035356e44881df0dd5c36be5f315e62036b5c77b
reydinerr <reydiner@protonmail.com>
2023-10-28 20:29:48 -0300

    feat: add basic structure intergration

---
+++ bin/e_commerce_aws.ts
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+import "source-map-support/register"
+import * as cdk from "aws-cdk-lib"
+import { ProductsAppStack } from "../lib/productsApp-stack"
+import { ECommerceApiStack } from "../lib/ecommerceApi-stack"
+import { ProductsAppLayersStack } from "../lib/productsAppLayers-stack"
+
+const app = new cdk.App()
+
+const env: cdk.Environment = {
+  account: "126027520545",
+  region: "sa-east-1"
+}
+
+const tags = {
+  cost: "ECommerce",
+  team: "ECommerceDev"
+}
+
+const productsAppLayersStack = new ProductsAppLayersStack(app, "ProductsAppLayers", {
+  tags: tags,
+  env: env
+})
+
+const productsAppStack = new ProductsAppStack(app, "ProductsApp", {
+  tags: tags,
+  env: env
+})
+productsAppStack.addDependency(productsAppLayersStack)
+
+const eCommerceApiStack = new ECommerceApiStack(app, "ECommerceApi", {
+  productsFetchHandler: productsAppStack.productsFetchHandler,
+  productsAdminHandler: productsAppStack.productsAdminHandler,
+  tags: tags,
+  env: env
+})
+eCommerceApiStack.addDependency(productsAppStack)
