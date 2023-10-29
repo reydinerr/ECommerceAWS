a2cd325eb92b1ca2e6c05144a0d860bdfc43ff13
reydinerr <reydiner@protonmail.com>
2023-10-28 21:57:05 -0300

    feat: enable lambda insights

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -49,7 +49,8 @@ export class ProductsAppStack extends cdk.Stack {
         PRODUCTS_DDB: this.productsDdb.tableName
       },
       layers: [productsLayer],
-      tracing: lambda.Tracing.ACTIVE
+      tracing: lambda.Tracing.ACTIVE,
+      insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_119_0
     })
     this.productsDdb.grantReadData(this.productsFetchHandler)
 
@@ -67,7 +68,8 @@ export class ProductsAppStack extends cdk.Stack {
         PRODUCTS_DDB: this.productsDdb.tableName
       },
       layers: [productsLayer],
-      tracing: lambda.Tracing.ACTIVE
+      tracing: lambda.Tracing.ACTIVE,
+      insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_119_0
     })
     this.productsDdb.grantWriteData(this.productsAdminHandler)
   }
